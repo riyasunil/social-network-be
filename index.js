b03b21855c66ae3fc80915cf5ec8853dc5946878
riyasunil <mdl21cs102@mec.ac.in>
2025-01-09 17:06:52 +0000

    new commit

--- index.js
+++ index.js
@@ -335,6 +335,6 @@ app.get("/api/feed", verifyToken, async (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, 'localhost', () => {
   console.log(`Server is running on port ${PORT}`);
 });
